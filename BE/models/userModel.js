19f230b407ce0624184912c4e4159c48b189f27e
HieuKyNang <bmdinhhieu@gmail.com>
2025-03-08 23:03:48 +0700

    userModel

--- BE/models/userModel.js
+++ BE/models/userModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    email2: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     cartData: { type: Object, default: {} }
 }, { minimize: false })
