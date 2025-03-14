4d58b1d7484a8ed8808c9508bdc9e98aec6614da
HieuKyNang <bmdinhhieu@gmail.com>
2025-03-14 23:07:57 +0700

    update registerUser

--- BE/controllers/userController.js
+++ BE/controllers/userController.js
@@ -1,4 +1,10 @@
-
+import userModel from "../models/userModel.js"
+import validator from "validator"
+import brcypt from "bcrypt"
+import jwt from "jsonwebtoken"
+const createToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET)
+}
 //route user login
 const loginUser = async (req, res) => {
 
@@ -6,7 +12,39 @@ const loginUser = async (req, res) => {
 
 //route user register
 const registerUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body
+        //checking user alredy exists or not
+        const exists = await userModel.findOne({ email })
+        if (exists) {
+            return res.json({ success: false, message: "User already exists" })
+        }
+        //validating email format & strong password
+        if (!validator.isEmail(email)) {
+            return res.json({ success: false, message: "Please enter a valid email" })
+        }
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Please enter a strong password" })
+        }
+        //hashing user password
+        const salt = await brcypt.genSalt(10)
+        const hashedPassword = await brcypt.hash(password, salt)
 
+        const newUser = new userModel({
+            name,
+            email,
+            password: hashedPassword
+        })
+
+        const user = await newUser.save()
+
+        const token = createToken(user._id)
+
+        res.json({ success: true, token })
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, massage: error.massage })
+    }
 }
 
 //route admin login
@@ -15,4 +53,4 @@ const adminLogin = async (req, res) => {
 }
 
 
-export { loginUser, registerUser }
+export { loginUser, registerUser, adminLogin }
--- BE/models/userModel.js
+++ BE/models/userModel.js
@@ -3,7 +3,6 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    email2: { type: String, required: true, unique: true },
     password: { type: String, required: true },
     cartData: { type: Object, default: {} }
 }, { minimize: false })
